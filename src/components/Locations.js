14576b80d9eec785764c689e7a795c37ee572bbf
Jessica Sutton <jmsutton0703@gmail.com>
2018-10-24 17:58:29 -0500

    Create React components

---
+++ src/components/Locations.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Locations extends Component {
+  render(){
+    return(
+      <div className="locations-container">
+        <p>This is the list of locations</p>
+      </div>
+    )
+  }
+};
+
+export default Locations;

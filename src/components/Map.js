2b09059ab35464a98b8cb9f26b1add7131d6ff18
Jessica Sutton <jmsutton0703@gmail.com>
2018-10-31 23:20:05 -0500

    Add map to component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class Map extends Component {
-  render(){
-    return(
+  static defaultProps = {
+    center: {
+      lat: 43.29,
+      lng: -0.37
+    },
+    zoom: 20
+  };
+
+  render() {
+    return (
       <div className="map-container">
-        <p>This is the map</p>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyACukLzS9_nvdIVFzuZ5RuOIzK4qTGdzo8' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+        <AnyReactComponent
+          lat={43.2951}
+          lng={-0.370797}
+          text={'Pau, France'}
+        />
+        </GoogleMapReact>
       </div>
-    )
+    );
   }
-};
+}
 
 export default Map;

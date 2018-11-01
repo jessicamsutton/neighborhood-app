4a20fd1cb312cb86aafcfc23da4b9d685290c55c
Jessica Sutton <jmsutton0703@gmail.com>
2018-10-31 23:32:54 -0500

    Add input element

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import {DebounceInput} from 'react-debounce-input';
+
 
 class Locations extends Component {
+  constructor() {
+    super();
+    this.state = {
+      value: '',
+    }
+  }
+
+
   render(){
     return(
       <div className="locations-container">
+
+        <div className="input-container">
+          <DebounceInput
+            type="text"
+            placeholder="Search locations..."
+            minLength={2}
+            debounceTimeout={300}
+            onChange={event => this.setState({value: event.target.value})}
+          />
+        </div>
+
         <p>This is the list of locations</p>
       </div>
     )

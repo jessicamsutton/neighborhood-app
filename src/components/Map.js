8cbd9f865d1c72d25a81b17a87386c10d06b7eef
Jessica Sutton <jmsutton0703@gmail.com>
2018-11-07 17:08:48 -0600

    Add markers to map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,20 +1,58 @@
 import React, { Component } from 'react';
-import '../data/locations.json';
+import {Map, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 class MapContainer extends Component {
-  constructor() {
-    super();
-    this.state = {
-    }
+  state = {
+    markers: this.props.allLocations,
+    activeMarker: null,
+    showingInfoWindow: false,
   }
+
+  onMarkerClick = (props, marker, e) => {
+    console.log(marker);
+
+    this.setState({
+      activeMarker: marker,
+      showingInfoWindow: true,
+    });
+  }
+
+  onMapClick = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  };
 
   render() {
     return (
       <main className="map-container">
-        <p>This is a map. Trust me.</p>
+        <Map
+          role="application"
+          aria-label="map"
+          google={this.props.google}
+          initialCenter={{ lat: this.props.lat, lng: this.props.lng }}
+          zoom={this.props.zoom}
+          onClick={this.onMapClick}
+        >
+
+          {this.state.markers.map((location, index) =>
+              <Marker
+                key={index}
+                name={location.name}
+                position={{ lat: location.pos.lat, lng: location.pos.lng }}
+                onClick={this.onMarkerClick}
+              />
+          )}
+
+        </Map>
       </main>
     );
   }
 }
 
-export default MapContainer;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyACukLzS9_nvdIVFzuZ5RuOIzK4qTGdzo8',
+})(MapContainer)

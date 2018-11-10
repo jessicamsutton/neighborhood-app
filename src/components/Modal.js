93ce370a785b69fb7a37fc204b7fe32d732cc105
Jessica Sutton <jmsutton0703@gmail.com>
2018-11-10 14:03:37 -0600

    Create modal component to replace infowindow

---
+++ src/components/Modal.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Modal extends Component {
+  render() {
+    return (
+      <div className="modal">
+        <div className="modal-content">
+          <h3>{this.props.activeMarker.name}</h3>
+          <img src={this.props.modalImage} alt={this.props.activeMarker.name} />
+          <p>Photo provided by Foursquare</p>
+          <button className="close-button" onClick={this.props.closeModal}>Close</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Modal;

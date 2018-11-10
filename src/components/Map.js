c243c2ce18245fe4ac73ebb30cd7e52dfe502302
Jessica Sutton <jmsutton0703@gmail.com>
2018-11-10 14:04:43 -0600

    Remove infowindow component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Map, Marker, InfoWindow, GoogleApiWrapper} from 'google-maps-react';
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 class MapContainer extends Component {
   render() {
@@ -10,34 +10,20 @@ class MapContainer extends Component {
           aria-label="map"
           google={this.props.google}
           className="map"
-          initialCenter={{ lat: 43.295, lng: -0.362 }}
-          zoom={14}
-          onClick={this.props.closeInfoWindow}
+          initialCenter={{ lat: 43.29, lng: -0.352 }}
+          zoom={13}
+          onClick={this.props.closeModal}
         >
-
           {this.props.filteredLocations.map((location, index) =>
-              <Marker
-                className="markers"
-                key={index}
-                name={location.name}
-                position={{ lat: location.pos.lat, lng: location.pos.lng }}
-                onClick={this.props.onMarkerClick}
-                animation={location.name === this.props.activeMarker.name ? this.props.google.maps.Animation.BOUNCE : {}}
-              />
+            <Marker
+              key={index}
+              name={location.name}
+              position={{ lat: location.position.lat, lng: location.position.lng }}
+              onClick={this.props.onMarkerClick}
+              animation={location.name === this.props.selectedName ?
+                this.props.google.maps.Animation.BOUNCE : {}}
+            />
           )}
-
-            <InfoWindow
-              className="info-window"
-              marker={this.props.activeMarker}
-              visible={this.props.showingInfoWindow}
-              onClose={this.props.closeInfoWindow} >
-                <div>
-                  <h3>{this.props.selectedPlace.name}</h3>
-                  <img src={this.props.infoWindowImage} alt={this.props.selectedPlace.name} />
-                  <p>Photo provided by Foursquare</p>
-                </div>
-            </InfoWindow>
-
         </Map>
       </main>
     );

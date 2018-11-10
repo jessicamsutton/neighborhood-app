599971de3738b61d87b4a84751b0a392d460b215
Jessica Sutton <jmsutton0703@gmail.com>
2018-11-10 02:51:38 -0600

    Add filterable list of locations to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import {DebounceInput} from 'react-debounce-input';
-import Locations from '../data/locations.json';
-
+import { DebounceInput } from 'react-debounce-input';
+import {GoogleApiWrapper} from 'google-maps-react';
 
 class Sidebar extends Component {
   constructor() {
@@ -12,30 +11,39 @@ class Sidebar extends Component {
   }
 
   render(){
-    return(
-      <aside className="sidebar">
+    return (
+      <div className="sidebar">
         <div className="input-container">
           <DebounceInput
+            className="input"
             type="text"
             value={this.state.query}
-            placeholder="Search locations..."
+            placeholder="Filter by location name"
             minLength={2}
-            debounceTimeout={300}
-            onChange={event => this.setState({value: event.target.value})}
+            debounceTimeout={500}
+            onChange={e => this.props.filterLocations(e.target.value)}
           />
         </div>
-        <ul>
-          {
-            Locations.map((location) => {
-              return  <li className="sidebar-list" key={location.key}>
-                        {location.name}
-                      </li>
-            })
-          }
-        </ul>
-      </aside>
+        <div>
+          <ul className="sidebar-list">
+            {this.props.filteredLocations.map((location, index) =>
+              <li
+                  key={index}
+                  name={location.name}
+                  position={{ lat: location.pos.lat, lng: location.pos.lng }}
+                  onClick={this.props.onListItemClick(location)}
+                  animation={location.name === this.props.activeMarker.name ? this.props.google.maps.Animation.BOUNCE : {}}
+              >
+                {location.name}
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
     )
   }
 };
 
-export default Sidebar;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyACukLzS9_nvdIVFzuZ5RuOIzK4qTGdzo8',
+})(Sidebar)
